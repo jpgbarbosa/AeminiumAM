0432454c3a7fab3dc49efcd55fb0fbb5f142ae98
Joo Barbosa <j.barbosa333@gmail.com>
2010-10-29 17:11:38 +0100

    merge problems.

---
+++ AeminiumAM/actor/IReact.java
@@ -0,0 +1,5 @@
+package actor;
+
+public interface IReact {
+	public void react();
+}
---
+++ AeminiumAM/main/Test.java
@@ -0,0 +1,41 @@
+package main;
+
+import actor.*;
+import aeminium.runtime.Runtime;
+import aeminium.runtime.examples.fjtests.AeminiumFibonacci;
+import aeminium.runtime.examples.fjtests.AeminiumFibonacci.FibBody;
+import aeminium.runtime.implementations.Factory;
+
+
+public class Test {
+	public static FibBody body;
+
+	public static class TestActor extends Actor {
+		int result;
+
+		public TestActor(Runtime rt) {
+			super(rt);
+		}
+		
+		@Override
+		public void react(){
+			System.out.println("Cenas");
+			result = 42;
+		}		
+
+	}
+
+	public static void main(String[] args) {
+
+		Runtime rt = Factory.getRuntime();
+		rt.init();
+		TestActor a = new TestActor(rt);
+		a.sendMessage();
+
+		rt.shutdown();
+	
+		System.out.println("ola: "+a.result);
+
+	}
+
+}
