9df52aebc584a82d3f4a1ea8b76704b26449b125
Joo Barbosa <j.barbosa333@gmail.com>
2011-06-22 15:48:57 +0100

    unused files deleted.

--- AeminiumAM/unused/readOnly.java
+++
@@ -1,9 +0,0 @@
-package unused;
-
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-
-@Retention(RetentionPolicy.RUNTIME)
-public @interface readOnly {
-	boolean isReadOnly() default true;
-}
